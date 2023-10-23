181d3c1d8dc31a65e259f391fc5e53d0bfe5b263
이원영 <sheltonwon@gmail.com>
2023-10-23 17:07:22 +0900

    feat: 사용자 entity 생성

---
+++ backend/member-service/src/main/java/com/codesmith/goojangmember/member/persistence/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.codesmith.goojangmember.member.persistence;
+
+import com.codesmith.goojangmember.member.persistence.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
