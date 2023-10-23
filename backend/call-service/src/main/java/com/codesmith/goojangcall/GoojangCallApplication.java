59fdd86d976aa4d4ba535a29614c9035c62d7e43
이원영 <sheltonwon@gmail.com>
2023-10-23 14:53:35 +0900

    init: 요청 서비스 프로젝트 추가

---
+++ backend/call-service/src/main/java/com/codesmith/goojangcall/GoojangCallApplication.java
@@ -0,0 +1,13 @@
+package com.codesmith.goojangcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoojangCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoojangCallApplication.class, args);
+	}
+
+}
---
+++ backend/call-service/src/test/java/com/codesmith/goojangcall/GoojangCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesmith.goojangcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoojangCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
