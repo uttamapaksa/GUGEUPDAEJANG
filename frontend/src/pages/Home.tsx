824b16cf710721571c92fecfe6c37e0a6dc28a1e
이시영 <swy05123@gmail.com>
2023-10-23 13:13:24 +0900

    feat: 프론트 프로젝트 기본 설정 S09P31B204-76

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import { styled } from "styled-components";
+
+const Container = styled.div`
+background-color: red;
+  align-items: center;
+  margin-left: 10px;
+`;
+const MainWrapper = styled.div`
+background-color: #ffae00;
+  display: flex;
+  align-items: center;
+  margin-top: 35px;
+`
+
+function Home() {
+  return (
+    <Container>
+      <MainWrapper>
+        main page
+      </MainWrapper>
+    </Container>
+  );
+}
+
+export default Home;
+
