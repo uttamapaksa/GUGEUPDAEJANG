db278ba17a3c70a21f5bfead352482218d1c61db
이원영 <sheltonwon@gmail.com>
2023-10-23 16:24:32 +0900

    init: 사용자 마이크로 서비스 프로젝트 생성

---
+++ backend/member-service/src/main/java/com/codesmith/goojangmember/GoojangMemberApplication.java
@@ -0,0 +1,13 @@
+package com.codesmith.goojangmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoojangMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoojangMemberApplication.class, args);
+	}
+
+}
