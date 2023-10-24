ffbfd93d77cf9ed09d0cc6b2002f33a9c4817d7c
이승종 <maeugel_rosh@naver.com>
2023-10-24 17:47:20 +0900

    test: STOMP 예시코드 [S09P31B204-172]

---
+++ backend/calling-service/src/main/java/com/codesmith/goojangcalling/calling/dto/Greeting.java
@@ -0,0 +1,16 @@
+package com.codesmith.goojangcalling.calling.dto;
+
+public class Greeting {
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
