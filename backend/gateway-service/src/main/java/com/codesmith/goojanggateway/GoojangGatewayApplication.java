901287d199d62384b9af93ead3dc9d9e11a97ecc
이원영 <sheltonwon@gmail.com>
2023-10-25 15:00:55 +0900

    init: gateway 프로젝트 추가

---
+++ backend/gateway-service/src/main/java/com/codesmith/goojanggateway/GoojangGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.codesmith.goojanggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoojangGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoojangGatewayApplication.class, args);
+	}
+
+}
