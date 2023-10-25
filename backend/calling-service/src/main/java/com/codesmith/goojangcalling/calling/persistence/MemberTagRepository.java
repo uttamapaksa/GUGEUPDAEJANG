4566cfa27d64347ab5515f56625e3096b02ae1e0
이승종 <maeugel_rosh@naver.com>
2023-10-25 17:58:36 +0900

    feat: 멤버별 태그 조회 작성 S09B31B204-189

--- backend/calling-service/src/main/java/com/codesmith/goojangcalling/calling/persistence/MemberTagRepository.java
+++ backend/calling-service/src/main/java/com/codesmith/goojangcalling/calling/persistence/MemberTagRepository.java
@@ -1,7 +1,16 @@
 package com.codesmith.goojangcalling.calling.persistence;
 
+import com.codesmith.goojangcalling.calling.dto.response.MemberTagResponse;
 import com.codesmith.goojangcalling.calling.persistence.domain.MemberTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MemberTagRepository extends JpaRepository<MemberTag, Long> {
+
+    @Query("select new com.codesmith.goojangcalling.calling.dto.response.MemberTagResponse(m.tag) " +
+            "from MemberTag m where m.memberId = :memberId")
+    List<MemberTagResponse> findByMemberId (@Param("memberId") Long memberId);
 }
