a1d71268d4c53cb56ce580f54f5e32a3fbcc2ab6
이원영 <sheltonwon@gmail.com>
2023-10-25 14:50:01 +0900

    init: discovery 서비스 추가

---
+++ backend/discovery-service/src/main/java/com/codesmith/goojangdiscovery/GoojangDiscoveryApplication.java
@@ -0,0 +1,16 @@
+package com.codesmith.goojangdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class GoojangDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoojangDiscoveryApplication.class, args);
+	}
+
+}
---
+++ backend/discovery-service/src/test/java/com/codesmith/goojangdiscovery/GoojangDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesmith.goojangdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoojangDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
