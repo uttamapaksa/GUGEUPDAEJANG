0cfc27c0f1226c2550c82dfae3aae7cc2847411f
이승종 <maeugel_rosh@naver.com>
2023-10-25 17:57:15 +0900

    test: 태그 전체조회 테스트 S09P31B204-186

---
+++ backend/calling-service/src/test/java/com/codesmith/goojangcalling/calling/persistence/TagRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.codesmith.goojangcalling.calling.persistence;
+
+import com.codesmith.goojangcalling.calling.persistence.domain.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class TagRepositoryTest {
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    @Test
+    void 태그조회_전체() throws Exception {
+        //given
+        //when
+        List<Tag> tagList = tagRepository.findAll();
+
+        //then
+        System.out.println(tagList.size());
+        assertThat(tagList.size()).isGreaterThan(0);
+    }
+}
