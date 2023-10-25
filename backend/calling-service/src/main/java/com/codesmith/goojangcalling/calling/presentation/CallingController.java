8b0cca4bcdc0d4bd9c768026b92a5c13069a89f3
이승종 <maeugel_rosh@naver.com>
2023-10-25 14:59:31 +0900

    rename: 요청 파라미터 수정 S09B31B204-90

--- backend/calling-service/src/main/java/com/codesmith/goojangcalling/calling/presentation/CallingController.java
+++ backend/calling-service/src/main/java/com/codesmith/goojangcalling/calling/presentation/CallingController.java
@@ -1,6 +1,6 @@
 package com.codesmith.goojangcalling.calling.presentation;
 
-import com.codesmith.goojangcalling.calling.dto.response.UploadResponse;
+import com.codesmith.goojangcalling.calling.dto.response.FileUploadResponse;
 import com.codesmith.goojangcalling.infra.aws.S3Client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +20,7 @@ public class CallingController {
     private final S3Client s3Client;
 
     @PostMapping("/upload")
-    public ResponseEntity<List<UploadResponse>> upload(@RequestParam("file") List<MultipartFile> multipartFile) throws Exception {
+    public ResponseEntity<List<FileUploadResponse>> upload(@RequestParam("files") List<MultipartFile> multipartFile) throws Exception {
         return ResponseEntity.ok(s3Client.uploadFIle(multipartFile));
     }
 }
