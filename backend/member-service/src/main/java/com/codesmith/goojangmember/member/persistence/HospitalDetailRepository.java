e164bde75b3f18b87fbf27abcfa1c69eec6421f8
김수연 <agew0423@daum.net>
2023-10-25 15:42:40 +0900

    feat: 직선거리가 범위 내에 해당하는 병원 목록 조회 S09P31B204-148

--- backend/member-service/src/main/java/com/codesmith/goojangmember/member/persistence/HospitalDetailRepository.java
+++ backend/member-service/src/main/java/com/codesmith/goojangmember/member/persistence/HospitalDetailRepository.java
@@ -3,5 +3,5 @@ package com.codesmith.goojangmember.member.persistence;
 import com.codesmith.goojangmember.member.persistence.domain.HospitalDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HospitalDetailRepository extends JpaRepository<HospitalDetail, Long> {
+public interface HospitalDetailRepository extends JpaRepository<HospitalDetail, Long>, HospitalDetailSupportRepository {
 }
