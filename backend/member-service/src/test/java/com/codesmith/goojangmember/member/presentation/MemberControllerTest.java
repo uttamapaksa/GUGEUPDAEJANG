65635fa35c8c8b35adae762fd33787eec00dee70
이원영 <sheltonwon@gmail.com>
2023-10-26 15:56:23 +0900

    test: 회원가입 컨트롤러 테스트 완료

--- backend/member-service/src/test/java/com/codesmith/goojangmember/member/presentation/MemberControllerTest.java
+++ backend/member-service/src/test/java/com/codesmith/goojangmember/member/presentation/MemberControllerTest.java
@@ -1,17 +1,25 @@
 package com.codesmith.goojangmember.member.presentation;
 
 import com.codesmith.goojangmember.member.application.MemberService;
+import com.codesmith.goojangmember.member.dto.request.HospitalJoinRequest;
+import com.codesmith.goojangmember.member.dto.request.ParamedicJoinRequest;
 import com.codesmith.goojangmember.member.exception.MemberNotFoundException;
 import com.codesmith.goojangmember.member.persistence.domain.Member;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MemberController.class)
@@ -51,4 +59,37 @@ class MemberControllerTest {
         perform
                 .andExpect(status().isNotFound());
     }
+
+    @DisplayName("병원 회원가입 요청을 한다")
+    @Test
+    void 병원_회원가입_요청을_한다() throws Exception {
+        HospitalJoinRequest hospitalJoinRequest = new HospitalJoinRequest("example@example.com", "password123", "Hospital Name", "profile.jpg", "HOSPITAL", "123-456-789", "987-654-321", "123 Main St, City, Country", 40.7128, -74.0060);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(hospitalJoinRequest);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/member/join/hospital")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody)
+                )
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("구급대원 회원가입 요청을 한다")
+    @Test
+    void 구급대원_회원가입_요청을_한다() throws Exception {
+        ParamedicJoinRequest paramedicJoinRequest = new ParamedicJoinRequest("paramedic@example.com", "password123", "Paramedic Name", "paramedic.jpg", "PARAMEDIC", 1L);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper.writeValueAsString(paramedicJoinRequest);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/member/join/paramedic")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody)
+                )
+                .andExpect(status().isOk());
+    }
+
 }
