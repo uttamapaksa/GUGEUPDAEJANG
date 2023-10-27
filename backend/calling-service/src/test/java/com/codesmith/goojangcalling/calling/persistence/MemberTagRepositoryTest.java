e46210400a4a38733cf387974f09da70fdd39443
이승종 <maeugel_rosh@naver.com>
2023-10-27 13:15:11 +0900

    test: 이미 생성된 사용자태그 테스트 S09P31B204-195

--- backend/calling-service/src/test/java/com/codesmith/goojangcalling/calling/persistence/MemberTagRepositoryTest.java
+++ backend/calling-service/src/test/java/com/codesmith/goojangcalling/calling/persistence/MemberTagRepositoryTest.java
@@ -1,14 +1,13 @@
 package com.codesmith.goojangcalling.calling.persistence;
 
+import com.codesmith.goojangcalling.calling.exception.DuplicateMemberTagException;
 import com.codesmith.goojangcalling.calling.persistence.domain.MemberTag;
 import com.codesmith.goojangcalling.calling.persistence.domain.Tag;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -51,6 +50,21 @@ class MemberTagRepositoryTest {
 
         assertThat(savedMemberTag.getTag()).isEqualTo(tag);
         assertThat(savedMemberTag.getMemberId()).isEqualTo(521L);
+    }
+
+    @DisplayName("이미 생성된 사용자태그면 예외를 반환한다.")
+    @Test
+    void 이미_생성된_사용자태그면_예외를_반환한다() throws Exception {
+        String inputTagName = "추락";
+        Long memberId = 521L;
+        Tag tag = tagRepository.findByName(inputTagName).orElseThrow();
+
+        assertThrows(DuplicateMemberTagException.class, () -> {
+            Optional<MemberTag> memberTagOptional = memberTagRepository.findByMemberIdAndTag(memberId, tag);
+            memberTagOptional.ifPresent(memberTag -> {
+                throw new DuplicateMemberTagException("이미 생성된 사용자 태그입니다.");
+            });
+        });
     }
 
     @DisplayName("추가할 태그가 존재하지 않으면 태그를 생성하고 사용자태그에 추가한다.")
