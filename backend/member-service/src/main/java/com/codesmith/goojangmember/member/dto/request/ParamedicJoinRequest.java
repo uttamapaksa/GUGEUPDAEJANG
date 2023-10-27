02e143309abce923b4401da8ea45948d9b91f734
이원영 <sheltonwon@gmail.com>
2023-10-27 15:26:42 +0900

    refactor: dto, entity 분리

--- backend/member-service/src/main/java/com/codesmith/goojangmember/member/dto/request/ParamedicJoinRequest.java
+++ backend/member-service/src/main/java/com/codesmith/goojangmember/member/dto/request/ParamedicJoinRequest.java
@@ -1,7 +1,5 @@
 package com.codesmith.goojangmember.member.dto.request;
 
-import com.codesmith.goojangmember.member.persistence.domain.Member;
-import com.codesmith.goojangmember.member.persistence.domain.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -14,14 +12,4 @@ public class ParamedicJoinRequest {
     private String imageUrl;
     private String role;
     private Long centerId;
-
-    public Member toMember() {
-        String email = this.email;
-        String password = this.password;
-        String name = this.name;
-        String imageUrl = this.imageUrl;
-        Role role = Role.valueOf(this.role);
-
-        return new Member(email, password, name, imageUrl, role);
-    }
 }
