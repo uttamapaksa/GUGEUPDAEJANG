e8f39953fd814180cc6180d81258e678201c5481
김준섭 <vacanter@naver.com>
2023-10-27 14:20:38 +0900

    feat: 병원 구급대 소켓 컴포넌트 S09P31B204-126

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,29 +1,39 @@
-import { createBrowserRouter } from "react-router-dom";
-import Root from "./Root";
-import Home from "./pages/Home";
-import ErrorComponent from "./components/ErrorComponent";
-import NotFound from "./pages/NotFound";
-import Main from "./pages/Hospital/Main/Main";
+import { createBrowserRouter } from 'react-router-dom';
+import Root from './Root';
+import Home from './pages/Home';
+import ErrorComponent from './components/ErrorComponent';
+import NotFound from './pages/NotFound';
+import Main from './pages/Hospital/Main/Main';
+import HospitalSocket from './sockets/HospitalSocket';
+import ParamedicSocket from './sockets/ParamedicSocket';
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        children:[
-            {
-                path: "",
-                element: <Home />,
-                errorElement: <ErrorComponent />,
-
-            },
-            {
-                path: "/hospital",
-                element: <Main />,
-                errorElement: <ErrorComponent />,
-
-            }
-        ],
-        errorElement: <NotFound />
-    }
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+        errorElement: <ErrorComponent />,
+      },
+      {
+        path: '/hospital',
+        element: <Main />,
+        errorElement: <ErrorComponent />,
+      },
+      {
+        path: '/hospitalSocket',
+        element: <HospitalSocket paramedicId='1'/>,
+        errorElement: <ErrorComponent />,
+      },
+      {
+        path: '/paramedicSocket',
+        element: <ParamedicSocket hospitalId='1'/>,
+        errorElement: <ErrorComponent />,
+      },
+    ],
+    errorElement: <NotFound />,
+  },
 ]);
 export default router;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    global: 'window'
+  },
+  server: {
+    host: true,
+    port: 5173,
+    https: {
+       key: 'privkey.pem',
+       cert: 'fullchain.pem',
+     },
+  },
   plugins: [react()],
 })
