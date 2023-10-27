2e03987f419b94a695b23e2dfb54880f2f4ccf4e
이시영 <swy05123@gmail.com>
2023-10-27 15:19:35 +0900

    feat: 지도 요소S09P31B204-121

--- frontend/src/components/libraries/Map/InfoWindow/InfoContents.tsx
+++ frontend/src/components/libraries/Map/InfoWindow/InfoContents.tsx
@@ -1,100 +1,54 @@
+import {
+  AddrP,
+  AddrTitle,
+  ContentsP,
+  ElapseTimeSpan,
+  InfoBox, 
+  InfoWindowArrow, 
+  InfoWindowContainer, 
+  KtasDiv, 
+  LeftTimeSpan, 
+  StartTimeSpan
+} from "./InfoContents.style";
+
+export interface InfoProps{
+  ktas: number;
+  addt: string;
+}
 //infowindow 내용 - 개선 예정
-function InfoContents() {
+function InfoContents(props:any) {
+
   return (
-    <div
-      style={{
-        position: "absolute",
-        bottom: 0,
-        left: "50%",
-        transform: "translate(-50%, 0)",
-        display: "flex",
-        backgroundColor: "#ffffffea",
-        boxShadow: "1px 5px 5px 5px #3b3b3b40",
-        fontSize: "12px",
-        borderRadius: "10px",
-        flexDirection: "column",
-        width: "250px",
-      }}
-    >
-      <div
-        style={{
-          position: "absolute",
-          display: "flex",
-          top: 0,
-          right: 0,
-          width: "60px",
-          height: "30px",
-          backgroundColor: "red",
-          borderRadius: "0 10px 0 5px",
-          color: "#ffffff",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        {"KTAS2"}
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          bottom: "-33px",
-          left: "50%",
-          transform: "translate(-50%, 0)",
-          width: 0,
-          height: 0,
-          borderBottom: "20px solid transparent",
-          borderTop: "20px solid #ffffffea",
-          borderLeft: "20px solid transparent",
-          borderRight: "20px solid transparent",
-        }}
-      ></div>
-      <div className="info-box" style={{ width: "100%" }}>
-        <p
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            marginBottom: "3px",
-            padding: "0 10px",
-            overflow: "hidden",
-          }}
-        >
-          <span className="tit" style={{ fontSize: "13px", fontWeight: "bold" }}>
-            {"대전 유성구 학하남로 10"}
-          </span>
-          <span className="new-addr">{11.5}km</span>
-        </p>
-        <p
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            padding: "0 10px",
-          }}
-        >
-          <span className="old-addr" style={{ color: "#707070", textAlign: "left", width: "50%" }}>
-            {"오전 01:34"}
-          </span>
-          <span className="old-addr" style={{ color: "#707070", textAlign: "right", width: "50%" }}>
-            요청 대기 {3}분 경과
-          </span>
-        </p>
+    <InfoWindowContainer>
+      <KtasDiv $num={'KTAS'+props.ktas}>
+        KTAS{props.ktas}
+      </KtasDiv>
+      <InfoWindowArrow />
+      <InfoBox>
+        <AddrP>
+          <AddrTitle>
+            {props.addr}
+          </AddrTitle>
+          <span>{11.5}km</span>
+        </AddrP>
+        <ContentsP>
+          <StartTimeSpan>
+            {props.requestAt}
+          </StartTimeSpan>
+          <ElapseTimeSpan>
+            요청 대기 {props.elapseMin}분 경과
+          </ElapseTimeSpan>
+        </ContentsP>
         <hr
           style={{ borderWidth: 0, height: "0.5px", width: "100%", backgroundColor: "#8a8a8a" }}
         />
-        <p
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            padding: "0 10px",
-          }}
-        >
-          <span
-            className="old-addr"
-            style={{ color: "#F44336", width: "100%", textAlign: "right" }}
-          >
-            {20}분 이내 도착 가능
-          </span>
-        </p>
-      </div>
-    </div>
+        <ContentsP>
+          <LeftTimeSpan>
+            {props.leftTime}분 이내 도착 가능
+          </LeftTimeSpan>
+        </ContentsP>
+      </InfoBox>
+    </InfoWindowContainer>
   );
 }
 
--- frontend/src/components/libraries/Map/Marker/ParamedicMarker.tsx
+++ frontend/src/components/libraries/Map/Marker/ParamedicMarker.tsx
@@ -1,38 +1,31 @@
 import { useEffect, useState } from "react";
 import { MapProps, ParamedicItem, Tmapv3 } from "../Map";
 import { ParamedicMarkerContainer } from "./ParamedicMarker.style";
-import { useRecoilValue } from "recoil";
-import { mapData } from "../mapAtom";
 
 function ParamedicMarker(props: any) {
-    const [parDictionary, setParDictionary] = useState<any>({});
-    // const map = useRecoilValue(mapData);
+    const [parList, setParList] = useState<any>([]);
+
     useEffect(() => {
-        console.log(props)
-        if(props.parList!==undefined && props.map!==undefined){
-            //parDictionary에서 props에는 없는 요소는 제거
-            //for()
-            
+        if (props.parList !== undefined && props.map !== undefined) {
+            for (let i = 0; i < parList.length; i++) {
+                parList[i].marker.setMap(null);
+            }
+            let next = []
             for (var i = 0; i < props.parList.length; i++) {
-                var lonlat =  new Tmapv3.LatLng(props.parList[i].pos.lat, props.parList[i].pos.lon);
+                var lonlat = new Tmapv3.LatLng(props.parList[i].pos.lat, props.parList[i].pos.lon);
                 var title = props.parList[i].name;
                 const size = new Tmapv3.Size(30, 30);
-                console.log(props.parList[i])
-                if(parDictionary.hasOwnProperty(props.parList[i].id)){
-                    parDictionary[props.parList[i].id].setPosition(lonlat);
-                }
-                else{
-                    const marker = new Tmapv3.Marker({
-                        position: lonlat,
-                        map: props.map,
-                        // color: positions[i].color,
-                        iconSize: size,
-                        // icon: props.parList[i].type,
-                        // label: title //Marker의 라벨.
-                    });
-                    setParDictionary({...parDictionary, [props.parList[i].id] : marker })
-                }
+                const marker = new Tmapv3.Marker({
+                    position: lonlat,
+                    map: props.map,
+                    // color: positions[i].color,
+                    iconSize: size,
+                    // icon: props.parList[i].type,
+                    // label: title //Marker의 라벨.
+                });
+                next.push({marker:marker});
             }
+            setParList(next);
         }
 
     }, [props]);
