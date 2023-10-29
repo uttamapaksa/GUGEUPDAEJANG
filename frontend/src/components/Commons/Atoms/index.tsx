240ff0e6902c6c979f80bc22970ff2480b2c1693
정현우 <gusehd502@naver.com>
2023-10-29 16:02:25 +0900

    feat: atom의 index 추가 S09P31B204-206

---
+++ frontend/src/components/Commons/Atoms/index.tsx
@@ -0,0 +1,92 @@
+import { 
+  BtnSubmit, 
+  BtnParaState, 
+  BtnKtas,
+  BtnToggle, 
+  BtnMediaRecord } from './Button';
+
+import {
+  DivKtasInfo,
+  DivTag,} from './Div'
+
+import {
+  ImgLogoHospital,
+  ImgMapMarkerHospital,
+  ImgMapMarkerAmbulance,
+  ImgTransferActivate,
+  ImgTransferDeactivate,
+  ImgHistoryActivate,
+  ImgHistoryDeactivate,
+  ImgStatisticActivate,
+  ImgStatisticDeactivate,
+  ImgSetupActivate,
+  ImgSetupDeactivate,
+  ImgAmbulance,
+  ImgRequestBell,
+  ImgCellphone,
+  ImgRecordVideoBlack,
+  ImgRecordVideoPink,
+  ImgRecordVoiceBlack,
+  ImgRecordVoicePink,
+  ImgArrowPinkRight,
+  ImgArrowBlackRight,
+  ImgLogo,
+  ImgLogoEmergencyRoom,
+  ImgMapMarker,
+  ImgArrowRight,
+  ImgArrowBottom,
+  ImgArrowLeft,
+  ImgArrowUp}from './Image'
+
+import {
+  IptUserInfo}from './Input'
+
+import{
+  TxtParamedicTitle,
+  TxtHeaderTitle,}from './Text'
+
+const A = {
+  BtnSubmit,
+  BtnParaState,
+  BtnKtas,
+  BtnToggle,
+  BtnMediaRecord,
+
+  DivKtasInfo,
+  DivTag,
+  
+  ImgLogoHospital,
+  ImgMapMarkerHospital,
+  ImgMapMarkerAmbulance,
+  ImgTransferActivate,
+  ImgTransferDeactivate,
+  ImgHistoryActivate,
+  ImgHistoryDeactivate,
+  ImgStatisticActivate,
+  ImgStatisticDeactivate,
+  ImgSetupActivate,
+  ImgSetupDeactivate,
+  ImgAmbulance,
+  ImgRequestBell,
+  ImgCellphone,
+  ImgRecordVideoBlack,
+  ImgRecordVideoPink,
+  ImgRecordVoiceBlack,
+  ImgRecordVoicePink,
+  ImgArrowPinkRight,
+  ImgArrowBlackRight,
+  ImgLogo,
+  ImgLogoEmergencyRoom,
+  ImgMapMarker,
+  ImgArrowRight,
+  ImgArrowBottom,
+  ImgArrowLeft,
+  ImgArrowUp,
+
+  IptUserInfo,
+
+  TxtParamedicTitle,
+  TxtHeaderTitle,
+};
+
+export default A;
