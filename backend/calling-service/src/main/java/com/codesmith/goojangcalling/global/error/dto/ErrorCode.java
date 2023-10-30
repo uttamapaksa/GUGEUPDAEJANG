1f815d88d6411baa5fb4a6d044c180fc75420445
이승종 <maeugel_rosh@naver.com>
2023-10-30 14:37:47 +0900

    fix: 에러코드 수정 S09P31B204-283

--- backend/calling-service/src/main/java/com/codesmith/goojangcalling/global/error/dto/ErrorCode.java
+++ backend/calling-service/src/main/java/com/codesmith/goojangcalling/global/error/dto/ErrorCode.java
@@ -1,17 +1,17 @@
 package com.codesmith.goojangcalling.global.error.dto;
 
 public enum ErrorCode {
-    WRONG_TAG(1001),
-    WRONG_MEMBER_TAG(1002),
-    EXISTS_MEMBER_TAG(1003);
+    WRONG_TAG("C2001"),
+    WRONG_MEMBER_TAG("C2002"),
+    EXISTS_MEMBER_TAG("C2003");
 
-    private final int value;
+    private final String value;
 
-    ErrorCode(int value) {
+    ErrorCode(String value) {
         this.value = value;
     }
 
-    public int getValue() {
+    public String  getValue() {
         return value;
     }
 }
