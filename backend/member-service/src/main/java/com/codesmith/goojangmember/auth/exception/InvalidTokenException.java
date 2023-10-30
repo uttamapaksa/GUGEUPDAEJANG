d5d40f0ed7bbfa560ec9bb289d88b585f089598d
이원영 <sheltonwon@gmail.com>
2023-10-30 15:50:15 +0900

    feat: jwt 토큰 생성 및 검증 provider 완성

---
+++ backend/member-service/src/main/java/com/codesmith/goojangmember/auth/exception/InvalidTokenException.java
@@ -0,0 +1,11 @@
+package com.codesmith.goojangmember.auth.exception;
+
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+
+    public InvalidTokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
