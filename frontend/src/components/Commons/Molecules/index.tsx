fa2db32a8f33cb18c45d32f3a2f692ac3917867f
정현우 <gusehd502@naver.com>
2023-10-31 14:22:04 +0900

    feat: 로그인 페이지 컴포넌트화 S09P31B204-293

---
+++ frontend/src/components/Commons/Molecules/index.tsx
@@ -0,0 +1,7 @@
+import Logo from "../Molecules/Logo/Logo"
+
+const M = {
+  Logo
+}
+
+export default M;
---
+++ frontend/src/components/Login/GoToGuest/GoToGuest.style.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* border: 3px solid green; */
+  height: 25%;
+`;
+
+export const BtnGoToGuest = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border-radius: 2vh;
+  border: none;
+
+  width: 80%;
+  height: 80%;
+  color: #ffffff;
+  font-size: 3vh;
+  background-color: #393d50;
+
+  @media (max-width: 500px) {
+    font-size: 2.2vh;
+  }
+`;
