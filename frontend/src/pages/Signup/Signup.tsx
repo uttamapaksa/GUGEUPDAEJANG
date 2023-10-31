92a5bc83debcf68a73837799babefaa685a09bc1
정현우 <gusehd502@naver.com>
2023-10-31 15:43:49 +0900

    feat: 회원가입 페이지 컴포넌트화 S09P31B204-294

--- frontend/src/pages/Signup/Signup.tsx
+++ frontend/src/pages/Signup/Signup.tsx
@@ -1,83 +1,14 @@
 import * as S from './Signup.style';
-import A from '/src/components/Commons/Atoms';
-import theme from '/src/styles';
+import M from '/src/components/Commons/Molecules'
+import SignupInput from '/src/components/Signup/SignupInput/SignupInput';
 
 function Signup() {
   return (
     <S.Container>
       <S.Wrapper>
         <S.ContentBox>
-
-          <S.Logo>
-            <A.ImgLogo $width="40%" />
-          </S.Logo>
-
-          <S.SignupInput>
-            <S.Row1>
-            <A.IptUserInfo
-              $width='70%'
-              $height='100%'
-              $fontSize='2vh' 
-              $boxShadow='0 0 0.3vh 0.3vh rgba(0, 0, 0, 0.10)'
-              placeholder='이메일'
-              >
-              </A.IptUserInfo>
-              <A.BtnSubmit
-              $margin='0 0 0 auto'
-              $width='20%'
-              $height='100%'
-              $fontSize='2vh'
-              $backgroundColor={theme.color.grayDarkest}
-              >
-                인증
-              </A.BtnSubmit>
-            </S.Row1>
-            <S.Row1>
-            <A.IptUserInfo
-              $width='100%'
-              $height='100%'
-              $fontSize='2vh' 
-              $boxShadow='0 0 0.3vh 0.3vh rgba(0, 0, 0, 0.10)'
-              placeholder='이름'
-              >
-              </A.IptUserInfo>
-            </S.Row1>
-            <S.Row1>
-            <A.IptUserInfo
-              $width='100%'
-              $height='100%'
-              $fontSize='2vh' 
-              $boxShadow='0 0 0.3vh 0.3vh rgba(0, 0, 0, 0.10)'
-              placeholder='비밀번호'
-              >
-              </A.IptUserInfo>
-            </S.Row1>
-            <S.Row1>
-            <A.IptUserInfo
-              $width='100%'
-              $height='100%'
-              $fontSize='2vh' 
-              $boxShadow='0 0 0.3vh 0.3vh rgba(0, 0, 0, 0.10)'
-              placeholder='비밀번호 확인'
-              >
-              </A.IptUserInfo>
-            </S.Row1>
-            <S.Row1>
-            <A.BtnSubmit
-              $margin='0 auto'
-              $width='95%'
-              $height='100%'
-              $fontSize='2vh'
-              $backgroundColor={theme.color.fontPink2}
-              >
-                회원 가입
-              </A.BtnSubmit>
-            </S.Row1>
-            <S.Row2>
-              <S.LoginToggle>회원가입 / 비밀번호 찾기</S.LoginToggle>
-            </S.Row2>
-            </S.SignupInput>
-
+          <M.Logo/>
+          <SignupInput/>
         </S.ContentBox>
       </S.Wrapper>
     </S.Container>
