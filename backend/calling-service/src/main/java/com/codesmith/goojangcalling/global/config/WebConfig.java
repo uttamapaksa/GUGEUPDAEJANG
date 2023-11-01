283ce8549d70295875948903bf8219c30f1859e9
이원영 <sheltonwon@gmail.com>
2023-11-01 21:45:58 +0900

    env: openfeign 추가

--- backend/calling-service/src/main/java/com/codesmith/goojangcalling/global/config/WebConfig.java
+++ backend/calling-service/src/main/java/com/codesmith/goojangcalling/global/config/WebConfig.java
@@ -2,10 +2,15 @@ package com.codesmith.goojangcalling.global.config;
 
 import com.codesmith.goojangcalling.global.passport.presentation.AuthMemberArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -18,4 +23,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authMemberArgumentResolver);
     }
+
+    @Bean
+    public HttpMessageConverters customConverters() {
+        List<HttpMessageConverter<?>> converters = new ArrayList<>();
+        converters.add(new MappingJackson2HttpMessageConverter());
+        return new HttpMessageConverters(converters);
+    }
+
 }
