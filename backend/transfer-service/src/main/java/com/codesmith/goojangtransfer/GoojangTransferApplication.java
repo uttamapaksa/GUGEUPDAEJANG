2b2a0d3d44ffa81d4cee821b3d0b053896fc897e
이승종 <maeugel_rosh@naver.com>
2023-11-01 16:01:02 +0900

    init: transfer 프로젝트 생성

---
+++ backend/transfer-service/src/main/java/com/codesmith/goojangtransfer/GoojangTransferApplication.java
@@ -0,0 +1,13 @@
+package com.codesmith.goojangtransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoojangTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoojangTransferApplication.class, args);
+	}
+
+}
