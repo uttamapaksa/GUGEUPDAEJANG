bc216d8fe3eb3d5a6a7ac8945a250c43040dee24
uttamapaksa <uttamapaksa@gmail.com>
2023-11-01 21:31:51 +0900

    feat259 confilct

--- frontend/src/pages/Signup/Signup.tsx
+++ frontend/src/pages/Signup/Signup.tsx
@@ -4,6 +4,8 @@ import M from '/src/components/Commons/Molecules'
 import SignupToggle from '/src/components/Signup/SignupToggle/SignupToggle';
 import HosSignupInput from '/src/components/Signup/HosSignupInput/HosSignupInput';
 import ParaSignupInput from '/src/components/Signup/ParaSignupInput/ParaSignupInput';
+// import SignupInput from '/src/components/Signup/SignupInput/SignupInput';
+import SearchModal from '/src/components/Signup/SearchModal/SearchModal';
 
 function Signup() {
   const [IsClick, setIsClick] = useState<boolean>(true)
@@ -18,6 +20,8 @@ function Signup() {
             setIsClick={setIsClick}/>
           {IsClick ? (<ParaSignupInput/> 
           ) : (<HosSignupInput/>)}
+          {/* <SignupInput/> */}
+          <SearchModal />
         </S.ContentBox>
       </S.Wrapper>
     </S.Container>
