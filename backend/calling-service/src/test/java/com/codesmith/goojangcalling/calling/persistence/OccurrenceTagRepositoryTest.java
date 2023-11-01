6f00adfc4517173733238e5c7fa11597784239ab
이승종 <maeugel_rosh@naver.com>
2023-11-01 22:31:20 +0900

    fix: test 코드 수정

--- backend/calling-service/src/test/java/com/codesmith/goojangcalling/calling/persistence/OccurrenceTagRepositoryTest.java
+++ backend/calling-service/src/test/java/com/codesmith/goojangcalling/calling/persistence/OccurrenceTagRepositoryTest.java
@@ -37,7 +37,7 @@ class OccurrenceTagRepositoryTest {
         List<FileUploadResponse> files = new ArrayList<>();
         files.add(new FileUploadResponse("https://codesmith-ggdj.s3.ap-northeast-2.amazonaws.com/62119bee-726d-4bd5-b6aa-07e65b39b951%EC%9C%A1%EA%B0%9C%EC%9E%A5.png", "image/png",122776L));
         tags = new ArrayList<>();
-        tags.add(new Tag(1L, "추락"));
+        tags.add(new Tag(7L, "추락"));
         memberId = 521L;
         callingCreateRequest = new CallingCreateRequest(KTAS.KTAS2, AgeGroup.YOUTH, Gender.MALE, "아파요", 35.123, 127.123, tags, files);
         occurrence = new Occurrence(memberId, callingCreateRequest.getKtas(), callingCreateRequest.getAgeGroup(), callingCreateRequest.getGender(), callingCreateRequest.getSymptom(), callingCreateRequest.getLatitude(), callingCreateRequest.getLongitude());
