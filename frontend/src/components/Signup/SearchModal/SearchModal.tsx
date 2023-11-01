ad5c6c4c9c7b97035b5712cdbc94451000d18961
uttamapaksa <uttamapaksa@gmail.com>
2023-11-02 07:51:38 +0900

    feat: 회원가입 검색 모달 S09P31B204-256

--- frontend/src/components/Signup/SearchModal/SearchModal.tsx
+++ frontend/src/components/Signup/SearchModal/SearchModal.tsx
@@ -1,14 +1,51 @@
-import { useState } from 'react';
 import * as S from './SearchModal.style';
-import Modal from 'react-modal';
+import A from '../../Commons/Atoms';
+import SearchList from '../SearchList/SearchList';
 
-function SearchModal() {
-  const [isOpen, setIsOpen] = useState(true);
+const dummy_results = [
+  ['대전 병원', '대전 대덕구 대전로 1215 대전병원', '042-625-0700'],
+  ['대전 병원', '대전 대덕구 대전로 1215 대전병원', '042-625-0700'],
+  ['대전 병원', '대전 대덕구 대전로 1215 대전병원', '042-625-0700'],
+  ['대전 병원', '대전 대덕구 대전로 1215 대전병원', '042-625-0700'],
+  ['대전 병원', '대전 대덕구 대전로 1215 대전병원', '042-625-0700'],
+  ['대전 병원', '대전 대덕구 대전로 1215 대전병원', '042-625-0700'],
+  ['대전 병원', '대전 대덕구 대전로 1215 대전병원', '042-625-0700'],
+  ['대전 병원', '대전 대덕구 대전로 1215 대전병원', '042-625-0700'],
+  ['대전 병원', '대전 대덕구 대전로 1215 대전병원', '042-625-0700'],
+];
 
+interface SearchModalProps {
+  isHosSearch: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function SearchModal({ isHosSearch, setIsOpen }: SearchModalProps) {
   return (
-    <Modal isOpen={isOpen} style={S.modalStyle}>
-      모달 내용
-    </Modal>
+    <S.Overlay>
+      <S.Content>
+        <S.ShadowTopDiv>
+          <S.Title>
+            <A.ImgExitBlack
+              onClick={() => setIsOpen(false)}
+              $position="absolute"
+              $zIndex="20"
+              $top="3vh"
+              $right="3vh"
+              $width="2.5vh"
+              $cursor="pointer"
+            />
+            <S.Title1>{isHosSearch ? '병원 검색' : '안전센터 검색'} </S.Title1>
+            <S.Title1>
+              <S.IptTitle placeholder="예) 유성구" />
+              <A.ImgExitGray $cursor="pointer" $margin="0 1vh" $width="2vh" />
+              <A.ImgSearchGray $cursor="pointer" $margin="0 1vh" $width="2.8vh" />
+            </S.Title1>
+          </S.Title>
+        </S.ShadowTopDiv>
+        <SearchList results={dummy_results} isHosSearch={isHosSearch} />
+        <S.ShadowBottomDiv />
+      </S.Content>
+    </S.Overlay>
   );
 }
 
