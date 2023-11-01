ba2433a7200dba8e9bae85d43bff5d58b8fa3afd
이원영 <sheltonwon@gmail.com>
2023-11-01 22:46:46 +0900

    feat: openfeign 클라이언트 등록

--- backend/calling-service/src/main/java/com/codesmith/goojangcalling/GoojangCallingApplication.java
+++ backend/calling-service/src/main/java/com/codesmith/goojangcalling/GoojangCallingApplication.java
@@ -4,9 +4,11 @@ import com.codesmith.goojangcalling.calling.presentation.CallingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class GoojangCallingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GoojangCallingApplication.class, args);
