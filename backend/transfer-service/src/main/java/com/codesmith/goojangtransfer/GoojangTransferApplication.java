320165d090b3a28c408751083ebbed1bba8b088f
이원영 <sheltonwon@gmail.com>
2023-11-01 22:45:00 +0900

    feat: openfeign 클라이언트 등록

--- backend/transfer-service/src/main/java/com/codesmith/goojangtransfer/GoojangTransferApplication.java
+++ backend/transfer-service/src/main/java/com/codesmith/goojangtransfer/GoojangTransferApplication.java
@@ -3,9 +3,11 @@ package com.codesmith.goojangtransfer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class GoojangTransferApplication {
 
 	public static void main(String[] args) {
