805774240524260e8909897f127e554c9aba062f
이승종 <maeugel_rosh@naver.com>
2023-11-01 14:01:53 +0900

    test: calling 저장 서비스

--- backend/calling-service/src/test/java/com/codesmith/goojangcalling/calling/application/CallingServiceImplTest.java
+++ backend/calling-service/src/test/java/com/codesmith/goojangcalling/calling/application/CallingServiceImplTest.java
@@ -3,6 +3,7 @@ package com.codesmith.goojangcalling.calling.application;
 import com.codesmith.goojangcalling.calling.dto.request.CallingCreateRequest;
 import com.codesmith.goojangcalling.calling.dto.response.FileUploadResponse;
 import com.codesmith.goojangcalling.calling.dto.response.HospitalSearchResponse;
+import com.codesmith.goojangcalling.calling.persistence.CallingRepository;
 import com.codesmith.goojangcalling.calling.persistence.OccurrenceFileRepository;
 import com.codesmith.goojangcalling.calling.persistence.OccurrenceRepository;
 import com.codesmith.goojangcalling.calling.persistence.OccurrenceTagRepository;
@@ -40,26 +41,42 @@ class CallingServiceImplTest {
     private OccurrenceTagRepository occurrenceTagRepository;
 
     @Mock
+    private CallingRepository callingRepository;
+
+    @Mock
     private HospitalClient hospitalClient;
 
     @InjectMocks
     private CallingServiceImpl callingService;
 
     private Occurrence occurrence;
-    private Long memberId;
+    private Long paramedicMemberId;
+    private Long hospitalMemberId;
     private CallingCreateRequest callingCreateRequest;
     private List<FileUploadResponse> files;
     private List<Tag> tags;
+    private Double latitude;
+    private Double longitude;
+    private Double distance;
+    private List<HospitalSearchResponse> hospitalList;
 
     @BeforeEach
     void setCallingRequest() {
         files = new ArrayList<>();
-        files.add(new FileUploadResponse("https://codesmith-ggdj.s3.ap-northeast-2.amazonaws.com/62119bee-726d-4bd5-b6aa-07e65b39b951%EC%9C%A1%EA%B0%9C%EC%9E%A5.png","image/png", 122776L));
+        files.add(new FileUploadResponse("https://codesmith-ggdj.s3.ap-northeast-2.amazonaws.com/62119bee-726d-4bd5-b6aa-07e65b39b951%EC%9C%A1%EA%B0%9C%EC%9E%A5.png", "image/png", 122776L));
         tags = new ArrayList<>();
         tags.add(new Tag(1L, "추락"));
-        memberId = 521L;
-        callingCreateRequest = new CallingCreateRequest(KTAS.KTAS2, AgeGroup.YOUTH, Gender.MALE, "아파요", 35.123, 127.123, tags, files);
-        occurrence = new Occurrence(memberId, callingCreateRequest.getKtas(), callingCreateRequest.getAgeGroup(), callingCreateRequest.getGender(), callingCreateRequest.getSymptom(), callingCreateRequest.getLatitude(), callingCreateRequest.getLongitude());
+        paramedicMemberId = 521L;
+        hospitalMemberId = 1040L;
+        latitude = 36.71;
+        longitude = 127.22;
+        distance = 10.0;
+        callingCreateRequest = new CallingCreateRequest(KTAS.KTAS2, AgeGroup.YOUTH, Gender.MALE, "아파요", latitude, longitude, tags, files);
+        occurrence = new Occurrence(paramedicMemberId, callingCreateRequest.getKtas(), callingCreateRequest.getAgeGroup(), callingCreateRequest.getGender(), callingCreateRequest.getSymptom(), callingCreateRequest.getLatitude(), callingCreateRequest.getLongitude());
+        hospitalList = Arrays.asList(
+                new HospitalSearchResponse(1040L, "A1800441", "세종충남대학교병원", "세종특별자치시 보듬7로 20, 세종충남대학교병원 (도담동)", "1800-3114", "044-995-3010", 10L, 9.8, 5L),
+                new HospitalSearchResponse(1041L, "A2302011", "재단법인베스티안재단베스티안병원", "충청북도 청주시 흥덕구 오송읍 오송생명1로 191-0", "043-910-7575", "043-904-8925", 7L, 11.5, 12L)
+        );
     }
 
     @DisplayName("사고, 사고파일, 사고태그를 저장한다.")
@@ -75,7 +92,9 @@ class CallingServiceImplTest {
         given(occurrenceFileRepository.saveAll(Mockito.any(List.class))).willReturn(occurrenceFileList);
         given(occurrenceTagRepository.saveAll(Mockito.any(List.class))).willReturn(occurrenceTagList);
 
-        callingService.addOccurrence(memberId, callingCreateRequest);
+        when(hospitalClient.searchHospital(latitude, longitude, distance)).thenReturn(Mono.just(hospitalList));
+
+        callingService.addOccurrence(paramedicMemberId, callingCreateRequest);
 
         verify(occurrenceRepository).save(Mockito.any(Occurrence.class));
         verify(occurrenceFileRepository).saveAll(Mockito.any(List.class));
@@ -85,16 +104,24 @@ class CallingServiceImplTest {
     @DisplayName("현재 위치로 수용가능한 병원들을 조회한다.")
     @Test
     public void 현재_위치로_수용가능한_병원들을_조회한다() throws Exception {
-        Double latitude = 35.6;
-        Double longitude = 127.22;
-        Double distance = 10.0;
-        List<HospitalSearchResponse> hospitalList = Arrays.asList(
-                new HospitalSearchResponse("A1800441", "세종충남대학교병원", "세종특별자치시 보듬7로 20, 세종충남대학교병원 (도담동)", "1800-3114", "044-995-3010", 10L),
-                new HospitalSearchResponse("A2302011", "재단법인베스티안재단베스티안병원","충청북도 청주시 흥덕구 오송읍 오송생명1로 191-0", "043-910-7575", "043-904-8925", 7L)
-        );
         when(hospitalClient.searchHospital(latitude, longitude, distance)).thenReturn(Mono.just(hospitalList));
 
         Mono<List<HospitalSearchResponse>> hospitalSearchResponses = callingService.searchHospital(latitude, longitude, distance);
         Assertions.assertEquals(hospitalList, hospitalSearchResponses.block());
+    }
+
+    @DisplayName("요청을 저장한다.")
+    @Test
+    void 요청을_저장한다() throws Exception {
+        List<Calling> callingList = Arrays.asList(
+                new Calling(occurrence, hospitalMemberId, Status.PENDING, null, ""),
+                new Calling(occurrence, hospitalMemberId, Status.PENDING, null, "")
+        );
+        given(callingRepository.saveAll(Mockito.any(List.class))).willReturn(callingList);
+        when(hospitalClient.searchHospital(latitude, longitude, distance)).thenReturn(Mono.just(hospitalList));
+
+        callingService.addCalling(callingCreateRequest, occurrence);
+
+        verify(callingRepository).saveAll(Mockito.any(List.class));
     }
 }
