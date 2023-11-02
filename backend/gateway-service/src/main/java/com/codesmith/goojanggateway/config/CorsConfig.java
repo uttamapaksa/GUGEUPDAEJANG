e4f65429f93d14b8a04a45aa33c6b61330d577f7
이원영 <sheltonwon@gmail.com>
2023-11-02 17:44:31 +0900

    fix: cors 경로 변경

--- backend/gateway-service/src/main/java/com/codesmith/goojanggateway/config/CorsConfig.java
+++ backend/gateway-service/src/main/java/com/codesmith/goojanggateway/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173", "https://localhost:5173", "https://k9b204.p.ssafy.io:443", "https://k9b204.p.ssafy.io"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173", "https://localhost:5173", "https://k9b204.p.ssafy.io:443", "https://k9b204.p.ssafy.io", "https://k9b204a.p.ssafy.io", "https://k9b204a.p.ssafy.io:443"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Authorization-refresh", "*"));
         config.setExposedHeaders(List.of("Authorization", "Authorization-refresh", "*"));
