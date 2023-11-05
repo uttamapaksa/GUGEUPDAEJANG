00e55d5a643ce8063198abd7eb6c503d695a5fc3
210-reverof <sheltonwon@gmail.com>
2023-11-06 02:37:03 +0900

    feat: 항목 추가

--- frontend/src/components/libraries/DataGrid/Datagrid.tsx
+++ frontend/src/components/libraries/DataGrid/Datagrid.tsx
@@ -2,49 +2,58 @@ import React, { useState, useCallback } from 'react'
 
 import ReactDataGrid from '@inovua/reactdatagrid-enterprise'
 import '@inovua/reactdatagrid-enterprise/index.css'
-
-import Button from '@inovua/reactdatagrid-community/packages/Button'
+import NumberFilter from '@inovua/reactdatagrid-community/NumberFilter'
+import SelectFilter from '@inovua/reactdatagrid-community/SelectFilter'
+import DateFilter from '@inovua/reactdatagrid-community/DateFilter'
 
 const DATASET_URL='https://localhost:64419/calling/history';
 
 const gridStyle = { minHeight: 600, marginTop: 10 };
+const GENDER = {
+  male: 'MALE',
+  femail: 'FEMALE'
+}
 
 const defaultFilterValue = [
-  { name: 'firstName', type: 'string', operator: 'contains', value: 'o' },
-  { name: 'lastName', type: 'string', operator: 'contains', value: '' },
-  { name: 'email', type: 'string', operator: 'contains', value: '' }
+  { name: 'id', type: 'string', operator: 'contains', value: '' },
+  { name: 'ageGroup', type: 'string', operator: 'contains', value: '' },
+  { name: 'gender', type: 'select', operator: 'eq', value: '' },
+  { name: 'tags', type: 'string', operator: 'contains', value: '' },
+  { name: 'address', type: 'string', operator: 'contains', value: '' },
+  { name: 'callingTime', type: 'string', operator: 'contains', value: '' },
+  { name: 'replyTime', type: 'string', operator: 'contains', value: '' },
+  { name: 'memberId', type: 'string', operator: 'contains', value: '' }
 ];
 
 const columns = [
-  { name: 'id', header: 'Id', defaultVisible: false, type: 'number', defaultWidth: 60 },
-  { name: 'firstName', header: 'First Name', defaultFlex: 1 },
-  { name: 'lastName', header: 'Last Name', defaultFlex: 1 },
-  { name: 'email', header: 'Email', groupBy: false, defaultFlex: 1 }
+  { name: 'id', header: '고유번호', defaultVisible: false, type: 'number', defaultWidth: 60 },
+  { name: 'ageGroup', header: '연령대', defaultFlex: 1 },
+  { name: 'gender', header: '성별', defaultFlex: 1, filterEditor: SelectFilter, filterEditorProps: {
+    placeholder: 'All',
+    dataSource: GENDER
+  },
+  render: ({ value })=> GENDER[value]? GENDER[value]: value },
+  { name: 'tags', header: '주요 분류', groupBy: false, defaultFlex: 1 },
+  { name: 'address', header: '주소', groupBy: false, defaultFlex: 1 },
+  { name: 'callingTime', header: '요청 시간', groupBy: false, defaultFlex: 1 },
+  { name: 'replyTime', header: '응답 시간', groupBy: false, defaultFlex: 1 },
+  { name: 'memberId', header: '담당자', groupBy: false, defaultFlex: 1 },
 ];
 
 const loadData = ({ skip, limit, sortInfo, groupBy, filterValue }:any) => {
   console.log(skip, limit, sortInfo, groupBy, filterValue);
-  return [{id:1, firstName:"hello", lastName:"lastlast", email:"email@eamil"}, {id:2, firstName:"sssecond", lastName:"my-last", email:"secdd@eamil"}]
+  console.log(filterValue[0]);
+
+  // TODO: 여기에 api 호출
+  const dummyData = [
+    { id: '3432', ageGroup: 'youth', gender: 'MALE', tags: '두통', address: '123 덕명동', callingTime: '9:00 AM', replyTime: '10:00 AM', memberId: '이시영' },
+    { id: '1234', ageGroup: 'adult', gender: 'FEMALE', tags: '과다출혈', address: '456 유성온천', callingTime: '10:30 AM', replyTime: '11:45 AM', memberId: '이승종' },
+    { id: '5678', ageGroup: 'senior', gender: 'MALE', tags: 'tag3', address: '789 금호동', callingTime: '2:00 PM', replyTime: '2:30 PM', memberId: '김수연' },
+    { id: '9876', ageGroup: 'youth', gender: 'FEMALE', tags: 'tag1', address: '321 봉명동', callingTime: '3:45 PM', replyTime: '4:15 PM', memberId: '김준섭' },
+    { id: '6543', ageGroup: 'adult', gender: 'MALE', tags: 'tag2', address: '654 궁동', callingTime: '5:30 PM', replyTime: '6:00 PM', memberId: '정현우' }
+  ];
   
-  // return fetch(DATASET_URL + '?skip='+skip+'&limit='+limit+(groupBy && groupBy.length?'&groupBy='+groupBy:'')+'&sortInfo='+JSON.stringify(sortInfo) + '&filterBy='+JSON.stringify(filterValue))
-  //   .then(response => {
-  //     console.log("===============data grid===============")
-  //     console.log(response.body)
-  //     const totalCountStr = response.headers.get('X-Total-Count');
-      
-  //     if (!totalCountStr) {
-  //       throw new Error("X-Total-Count header is missing");
-  //     }
-      
-  //     const totalCount = parseInt(totalCountStr, 10);
-  //     if (isNaN(totalCount)) {
-  //       throw new Error("X-Total-Count header is not a valid number");
-  //     }
-      
-  //     return response.json().then(data => {
-  //       return { data, count: totalCount };
-  //     })
-  //   })
+  return dummyData;
 }
 
 const DataGrid = () => {
