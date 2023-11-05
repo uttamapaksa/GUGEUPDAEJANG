36b301ba94c07e0d5860f73bbc6d7b488cf2625d
이승종 <maeugel_rosh@naver.com>
2023-11-05 16:06:00 +0900

    fix: 오타 수정

--- backend/calling-service/src/main/java/com/codesmith/goojangcalling/calling/dto/message/StatusChangeMessage.java
+++ backend/calling-service/src/main/java/com/codesmith/goojangcalling/calling/dto/message/StatusChangeMessage.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class StatusChangeMessage {
     private Long memberId;
-    private Long CallingId;
+    private Long callingId;
     private Status status;
     private String reason;
 }
