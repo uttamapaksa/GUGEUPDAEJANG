fec5dc005f44a695445c5bd478197a7aefb69e7a
이원영 <sheltonwon@gmail.com>
2023-11-06 16:32:58 +0900

    feat: 데이터 그리드 추가하기 S09P31B204-272

--- frontend/src/components/libraries/DataGrid/Datagrid.tsx
+++ frontend/src/components/libraries/DataGrid/Datagrid.tsx
@@ -1,56 +1,61 @@
 import React, { useState, useCallback } from 'react'
 
-import ReactDataGrid from '@inovua/reactdatagrid-enterprise'
-import '@inovua/reactdatagrid-enterprise/index.css'
-import NumberFilter from '@inovua/reactdatagrid-community/NumberFilter'
+import ReactDataGrid from '@inovua/reactdatagrid-community'
+import '@inovua/reactdatagrid-community/index.css'
 import SelectFilter from '@inovua/reactdatagrid-community/SelectFilter'
 import DateFilter from '@inovua/reactdatagrid-community/DateFilter'
-
-const DATASET_URL='https://localhost:64419/calling/history';
+import moment from 'moment'
 
 const gridStyle = { minHeight: 600, marginTop: 10 };
-const GENDER = {
-  male: 'MALE',
-  femail: 'FEMALE'
-}
+const GENDER: {[key: string]: string} = { MALE: '남성', FEMALE: '여성'};
+const gender = [{id: 'MALE', label: '남성'}, {id: 'FEMALE', label: '여성'}];
+const AGEGROUP: {[key: string]: string} = { INFANT: '영유아', CHILD: '어린이', ADOLESCENT: '청소년', YOUTH: '청년', MIDDLE: '중장년', SENIOR: '노년'}
+const ageGroup = [{id: 'INFANT', label: '영유아'}, {id: 'CHILD', label: '어린이'}, {id: 'ADOLESCENT', label: '청소년'}, {id: 'YOUTH', label: '청년'}, {id: 'MIDDLE', label: '중장년'}, {id: 'SENIOR', label: '노년'}];
+const ktas = [{id: 'KTAS1', label: 'KTAS1'}, {id: 'KTAS2', label: 'KTAS2'}, {id: 'KTAS3', label: 'KTAS3'}, {id: 'KTAS4', label: 'KTAS4'}, {id: 'KTAS5', label: 'KTAS5'}]
 
 const defaultFilterValue = [
   { name: 'id', type: 'string', operator: 'contains', value: '' },
-  { name: 'ageGroup', type: 'string', operator: 'contains', value: '' },
+  { name: 'ageGroup', type: 'select', operator: 'eq', value: '' },
   { name: 'gender', type: 'select', operator: 'eq', value: '' },
   { name: 'tags', type: 'string', operator: 'contains', value: '' },
   { name: 'address', type: 'string', operator: 'contains', value: '' },
-  { name: 'callingTime', type: 'string', operator: 'contains', value: '' },
-  { name: 'replyTime', type: 'string', operator: 'contains', value: '' },
-  { name: 'memberId', type: 'string', operator: 'contains', value: '' }
+  { name: 'callingTime', type: 'date', operator: 'inrange', value: '' },
+  { name: 'replyTime', type: 'date', operator: 'inrange', value: '' },
+  { name: 'ktas', type: 'select', operator: 'eq', value: '' }
 ];
 
 const columns = [
-  { name: 'id', header: '고유번호', defaultVisible: false, type: 'number', defaultWidth: 60 },
-  { name: 'ageGroup', header: '연령대', defaultFlex: 1 },
-  { name: 'gender', header: '성별', defaultFlex: 1, filterEditor: SelectFilter, filterEditorProps: {
-    placeholder: 'All',
-    dataSource: GENDER
+  { name: 'id', header: '고유번호', type: 'number', defaultFlex: 1 },
+  { name: 'ageGroup', header: '연령대', defaultFlex: 1, filterEditor: SelectFilter, filterEditorProps: { placeholder: 'All', dataSource: ageGroup }, render: ({ value }: { value: string })=> AGEGROUP[value]},
+  { name: 'gender', header: '성별', defaultFlex: 1, filterEditor: SelectFilter, filterEditorProps: { placeholder: 'All', dataSource: gender }, render: ({ value }: { value: string })=> GENDER[value] },
+  { name: 'tags', header: '주요 분류', defaultFlex: 1 },
+  { name: 'address', header: '주소', defaultFlex: 3 },
+  { name: 'callingTime', header: '요청 시간', type: 'date', defaultFlex: 2, dateFormat: 'YYYY/MM/DD HH:mm A', filterEditor: DateFilter, 
+    filterEditorProps: (_props:any, { index } : {index:number}) => { return { placeholder: index == 1? '시작': '끝' }},
+    render: ({ value, cellProps: { dateFormat } } : {value:any, cellProps:any}) => moment(value).format(dateFormat), 
   },
-  render: ({ value })=> GENDER[value]? GENDER[value]: value },
-  { name: 'tags', header: '주요 분류', groupBy: false, defaultFlex: 1 },
-  { name: 'address', header: '주소', groupBy: false, defaultFlex: 1 },
-  { name: 'callingTime', header: '요청 시간', groupBy: false, defaultFlex: 1 },
-  { name: 'replyTime', header: '응답 시간', groupBy: false, defaultFlex: 1 },
-  { name: 'memberId', header: '담당자', groupBy: false, defaultFlex: 1 },
+  { name: 'replyTime', header: '응답 시간', type: 'date', defaultFlex: 2, dateFormat: 'YYYY/MM/DD HH:mm A', filterEditor: DateFilter, 
+    filterEditorProps: (_props:any, { index } : {index:number}) => { return { placeholder: index == 1? '시작': '끝' }},
+    render: ({ value, cellProps: { dateFormat } } : {value:any, cellProps:any}) => moment(value).format(dateFormat), 
+  },
+
+  { name: 'ktas', header: 'KTAS', filterEditor: SelectFilter, filterEditorProps: { placeholder: 'All', dataSource: ktas }, render: ({ value }: { value: string })=> value},
 ];
 
 const loadData = ({ skip, limit, sortInfo, groupBy, filterValue }:any) => {
+  console.log("============Datagrid.tsx에서 콘솔 삭제==========");
   console.log(skip, limit, sortInfo, groupBy, filterValue);
-  console.log(filterValue[0]);
+  console.log(filterValue);
+  console.log("------------------------------------------------");
+  console.log("이걸로 요청 보내기 ~/calling/history?skip=" + skip + "&limit=" + limit + "&sortInfo=" + JSON.stringify(sortInfo) + "&groupBy=" + JSON.stringify(groupBy) + "&filterValue=" + JSON.stringify(filterValue));
+  console.log("===============================================");
 
-  // TODO: 여기에 api 호출
   const dummyData = [
-    { id: '3432', ageGroup: 'youth', gender: 'MALE', tags: '두통', address: '123 덕명동', callingTime: '9:00 AM', replyTime: '10:00 AM', memberId: '이시영' },
-    { id: '1234', ageGroup: 'adult', gender: 'FEMALE', tags: '과다출혈', address: '456 유성온천', callingTime: '10:30 AM', replyTime: '11:45 AM', memberId: '이승종' },
-    { id: '5678', ageGroup: 'senior', gender: 'MALE', tags: 'tag3', address: '789 금호동', callingTime: '2:00 PM', replyTime: '2:30 PM', memberId: '김수연' },
-    { id: '9876', ageGroup: 'youth', gender: 'FEMALE', tags: 'tag1', address: '321 봉명동', callingTime: '3:45 PM', replyTime: '4:15 PM', memberId: '김준섭' },
-    { id: '6543', ageGroup: 'adult', gender: 'MALE', tags: 'tag2', address: '654 궁동', callingTime: '5:30 PM', replyTime: '6:00 PM', memberId: '정현우' }
+    { id: '3432', ageGroup: 'INFANT', gender: 'FEMALE', tags: '두통', address: '123 덕명동', callingTime: '2023/02/15 9:00 AM', replyTime: '2023/02/15 10:00 AM', ktas: 'KTAS3' },
+    { id: '1234', ageGroup: 'YOUTH', gender: 'MALE', tags: '과다출혈', address: '456 유성온천', callingTime: '2023/02/15 10:30 AM', replyTime: '2023/02/15 11:45 AM', ktas: 'KTAS4' },
+    { id: '5678', ageGroup: 'SENIOR', gender: 'FEMALE', tags: 'tag3', address: '789 금호동', callingTime: '2023/10/11 2:00 PM', replyTime: '2023/06/15 2:30 PM', ktas: 'KTAS2' },
+    { id: '9876', ageGroup: 'INFANT', gender: 'MALE', tags: 'tag1', address: '321 봉명동', callingTime: '2023/04/16 3:45 PM', replyTime: '2023/04/15 4:15 PM', ktas: 'KTAS1' },
+    { id: '6543', ageGroup: 'ADOLESCENT', gender: 'MALE', tags: 'tag2', address: '654 궁동', callingTime: '2023/11/03 5:30 PM', replyTime: '2023/03/03 6:00 PM', ktas: 'KTAS3' }
   ];
   
   return dummyData;
@@ -58,8 +63,7 @@ const loadData = ({ skip, limit, sortInfo, groupBy, filterValue }:any) => {
 
 const DataGrid = () => {
   const [filterValue, setFilterValue] = useState(defaultFilterValue);
-  const [sortInfo, setSortInfo] = useState([]);
-
+  const [, setSortInfo] = useState([]);
   const dataSource = useCallback(loadData, [])
 
   return (
@@ -74,8 +78,8 @@ const DataGrid = () => {
         defaultGroupBy={[]}
         pagination
         dataSource={dataSource}
-        onSortInfoChange={setSortInfo}
-        onFilterValueChange={setFilterValue}
+        onSortInfoChange={()=>setSortInfo}
+        onFilterValueChange={()=>setFilterValue}
       />
     </div>
   );
