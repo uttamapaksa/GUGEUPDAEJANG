c5dc2aba470ee3c98a780ae83507da95642b7876
이원영 <sheltonwon@gmail.com>
2023-11-08 17:58:35 +0900

    refactor: totalCount 추가

--- backend/calling-service/src/main/java/com/codesmith/goojangcalling/calling/application/HistoryServiceImpl.java
+++ backend/calling-service/src/main/java/com/codesmith/goojangcalling/calling/application/HistoryServiceImpl.java
@@ -3,19 +3,16 @@ package com.codesmith.goojangcalling.calling.application;
 import com.codesmith.goojangcalling.calling.dto.FilterValue;
 import com.codesmith.goojangcalling.calling.dto.SortInfo;
 import com.codesmith.goojangcalling.calling.dto.request.CallingListRequest;
+import com.codesmith.goojangcalling.calling.dto.response.CallingItemResponse;
 import com.codesmith.goojangcalling.calling.dto.response.CallingListResponse;
 import com.codesmith.goojangcalling.calling.persistence.CallingRepository;
 import com.codesmith.goojangcalling.calling.persistence.domain.CallingItem;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.joda.time.format.DateTimeFormat;
 import org.springframework.stereotype.Service;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.logging.Filter;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,12 +21,13 @@ public class HistoryServiceImpl implements HistoryService {
     private final CallingRepository callingRepository;
     private final ObjectMapper objectMapper;
     @Override
-    public List<CallingListResponse> getCallingList(CallingListRequest callingHistoryRequest) {
+    public CallingListResponse getCallingList(CallingListRequest callingHistoryRequest) {
         SortInfo sortInfo = getSortInfo(callingHistoryRequest.getSortInfo());
         FilterValue[] filterValues = getFilterValues(callingHistoryRequest.getFilterValue());
 
+        Long totalCount = 31L;
         List<CallingItem> callings = callingRepository.findAllCallingByOptions(callingHistoryRequest.getSkip(), callingHistoryRequest.getLimit(), sortInfo, filterValues);
-        return convertToCallingListResponse(callings);
+        return new CallingListResponse(convertToCallingListResponse(callings), totalCount);
     }
 
     private FilterValue[] getFilterValues(String filterStr) {
@@ -54,12 +52,12 @@ public class HistoryServiceImpl implements HistoryService {
         return sortInfo;
     }
 
-    private List<CallingListResponse> convertToCallingListResponse(List<CallingItem> callingItemList) {
+    private List<CallingItemResponse> convertToCallingListResponse(List<CallingItem> callingItemList) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd hh:mm a");
 
         System.out.println("=====================" + callingItemList.get(0));
         return callingItemList.stream()
-                .map(callingItem -> new CallingListResponse(
+                .map(callingItem -> new CallingItemResponse(
                         callingItem.getId(),
                         callingItem.getAgeGroup().name(),
                         callingItem.getGender().name(),
