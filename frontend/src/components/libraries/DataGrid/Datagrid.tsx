84f002cca80c14f0deba8552ee07b8a23e3f9109
이원영 <sheltonwon@gmail.com>
2023-11-08 17:33:56 +0900

    feat: api 통신 완료

--- frontend/src/components/libraries/DataGrid/Datagrid.tsx
+++ frontend/src/components/libraries/DataGrid/Datagrid.tsx
@@ -5,6 +5,23 @@ import '@inovua/reactdatagrid-community/index.css'
 import SelectFilter from '@inovua/reactdatagrid-community/SelectFilter'
 import DateFilter from '@inovua/reactdatagrid-community/DateFilter'
 import moment from 'moment'
+import axios from 'axios'
+import { getHistoryList } from '/src/apis/history'
+
+const data = [
+  { id: '3432', ageGroup: 'INFANT', gender: 'FEMALE', tags: '두통', address: '123 덕명동', callingTime: '2023/02/15 9:00 AM', replyTime: '2023/02/15 10:00 AM', status: 'APPROVED', ktas: 'KTAS3' },
+  { id: '1234', ageGroup: 'YOUTH', gender: 'MALE', tags: '과다출혈', address: '456 유성온천', callingTime: '2023/02/15 10:30 AM', replyTime: '2023/02/15 11:45 AM', status: 'REJECTED', ktas: 'KTAS4' },
+  { id: '5678', ageGroup: 'SENIOR', gender: 'FEMALE', tags: 'tag3', address: '789 금호동', callingTime: '2023/10/11 2:00 PM', replyTime: '2023/06/15 2:30 PM', status: 'PENDING', ktas: 'KTAS2' },
+  { id: '9876', ageGroup: 'INFANT', gender: 'MALE', tags: 'tag1', address: '321 봉명동', callingTime: '2023/04/16 3:45 PM', replyTime: '2023/04/15 4:15 PM', status: 'TERMINATED', ktas: 'KTAS1' },
+  { id: '6543', ageGroup: 'ADOLESCENT', gender: 'MALE', tags: 'tag2', address: '654 궁동', callingTime: '2023/11/03 5:30 PM', replyTime: '2023/03/03 6:00 PM', status: 'FIXED', ktas: 'KTAS3' },
+  { id: '3432', ageGroup: 'INFANT', gender: 'FEMALE', tags: '두통', address: '123 덕명동', callingTime: '2023/02/15 9:00 AM', replyTime: '2023/02/15 10:00 AM', status: 'APPROVED', ktas: 'KTAS3' },
+  { id: '1234', ageGroup: 'YOUTH', gender: 'MALE', tags: '과다출혈', address: '456 유성온천', callingTime: '2023/02/15 10:30 AM', replyTime: '2023/02/15 11:45 AM', status: 'CANCELED', ktas: 'KTAS4' },
+  { id: '5678', ageGroup: 'SENIOR', gender: 'FEMALE', tags: 'tag3', address: '789 금호동', callingTime: '2023/10/11 2:00 PM', replyTime: '2023/06/15 2:30 PM', status: 'APPROVED', ktas: 'KTAS2' },
+  { id: '9876', ageGroup: 'INFANT', gender: 'MALE', tags: 'tag1', address: '321 봉명동', callingTime: '2023/04/16 3:45 PM', replyTime: '2023/04/15 4:15 PM', status: 'APPROVED', ktas: 'KTAS1' },
+  { id: '1234', ageGroup: 'YOUTH', gender: 'MALE', tags: '과다출혈', address: '456 유성온천', callingTime: '2023/02/15 10:30 AM', replyTime: '2023/02/15 11:45 AM', status: 'APPROVED', ktas: 'KTAS4' },
+  { id: '5678', ageGroup: 'SENIOR', gender: 'FEMALE', tags: 'tag3', address: '789 금호동', callingTime: '2023/10/11 2:00 PM', replyTime: '2023/06/15 2:30 PM', status: 'APPROVED', ktas: 'KTAS2' },
+  { id: '9876', ageGroup: 'INFANT', gender: 'MALE', tags: 'tag1', address: '321 봉명동', callingTime: '2023/04/16 3:45 PM', replyTime: '2023/04/15 4:15 PM', status: 'APPROVED', ktas: 'KTAS1' }
+];
 
 const gridStyle = { minHeight: 600, marginTop: 10 };
 const GENDER: { [key: string]: string } = { MALE: '남성', FEMALE: '여성' };
@@ -14,8 +31,6 @@ const ageGroup = [{ id: 'INFANT', label: '영유아' }, { id: 'CHILD', label: '�
 const STATUS: { [key: string]: string } = { APPROVED: '승인', REJECTED: '거절', PENDING: '대기', TERMINATED: '철회', FIXED: '확정', CANCELED: '취소' }
 const status = [{ id: 'APPROVED', label: '승인' }, { id: 'REJECTED', label: '거절' }, { id: 'PENDING', label: '대기' }, { id: 'TERMINATED', label: '철회' }, { id: 'FIXED', label: '확정' }, { id: 'CANCELED', label: '취소' }];
 const ktas = [{ id: 'KTAS1', label: 'KTAS1' }, { id: 'KTAS2', label: 'KTAS2' }, { id: 'KTAS3', label: 'KTAS3' }, { id: 'KTAS4', label: 'KTAS4' }, { id: 'KTAS5', label: 'KTAS5' }]
-
-// APPROVED, REJECTED, PENDING, TERMINATED, FIXED, CANCELED
 
 const defaultFilterValue = [
   { name: 'id', type: 'string', operator: 'contains', value: '' },
@@ -49,30 +64,13 @@ const columns = [
   { name: 'ktas', header: 'KTAS', filterEditor: SelectFilter, filterEditorProps: { placeholder: 'All', dataSource: ktas }, render: ({ value }: { value: string }) => value },
 ];
 
-const loadData = ({ skip, limit, sortInfo, groupBy, filterValue }: any) => {
-  console.log("============Datagrid.tsx에서 콘솔 삭제==========");
-  console.log(skip, limit, sortInfo, groupBy, filterValue);
-  console.log(filterValue);
-  console.log("------------------------------------------------");
-  console.log("이걸로 요청 보내기 ~/calling/history?skip=" + skip + "&limit=" + limit + "&sortInfo=" + JSON.stringify(sortInfo) + "&groupBy=" + JSON.stringify(groupBy) + "&filterValue=" + JSON.stringify(filterValue));
-  console.log("===============================================");
+const loadData = async ({ skip, limit, sortInfo, groupBy, filterValue }: any) => {
+  const encodedSortInfo = encodeURIComponent(JSON.stringify(sortInfo));
+  const encodedFilterValue = encodeURIComponent(JSON.stringify(filterValue));
+  const uri = "/calling?skip=" + skip + "&limit=" + limit + "&sortInfo=" + encodedSortInfo + "&groupBy=" + JSON.stringify(groupBy) + "&filterValue=" + encodedFilterValue;
+  const response = await getHistoryList(uri);
 
-  const data = [
-    { id: '3432', ageGroup: 'INFANT', gender: 'FEMALE', tags: '두통', address: '123 덕명동', callingTime: '2023/02/15 9:00 AM', replyTime: '2023/02/15 10:00 AM', status: 'APPROVED', ktas: 'KTAS3' },
-    { id: '1234', ageGroup: 'YOUTH', gender: 'MALE', tags: '과다출혈', address: '456 유성온천', callingTime: '2023/02/15 10:30 AM', replyTime: '2023/02/15 11:45 AM', status: 'REJECTED', ktas: 'KTAS4' },
-    { id: '5678', ageGroup: 'SENIOR', gender: 'FEMALE', tags: 'tag3', address: '789 금호동', callingTime: '2023/10/11 2:00 PM', replyTime: '2023/06/15 2:30 PM', status: 'PENDING', ktas: 'KTAS2' },
-    { id: '9876', ageGroup: 'INFANT', gender: 'MALE', tags: 'tag1', address: '321 봉명동', callingTime: '2023/04/16 3:45 PM', replyTime: '2023/04/15 4:15 PM', status: 'TERMINATED', ktas: 'KTAS1' },
-    { id: '6543', ageGroup: 'ADOLESCENT', gender: 'MALE', tags: 'tag2', address: '654 궁동', callingTime: '2023/11/03 5:30 PM', replyTime: '2023/03/03 6:00 PM', status: 'FIXED', ktas: 'KTAS3' },
-    { id: '3432', ageGroup: 'INFANT', gender: 'FEMALE', tags: '두통', address: '123 덕명동', callingTime: '2023/02/15 9:00 AM', replyTime: '2023/02/15 10:00 AM', status: 'APPROVED', ktas: 'KTAS3' },
-    { id: '1234', ageGroup: 'YOUTH', gender: 'MALE', tags: '과다출혈', address: '456 유성온천', callingTime: '2023/02/15 10:30 AM', replyTime: '2023/02/15 11:45 AM', status: 'CANCELED', ktas: 'KTAS4' },
-    { id: '5678', ageGroup: 'SENIOR', gender: 'FEMALE', tags: 'tag3', address: '789 금호동', callingTime: '2023/10/11 2:00 PM', replyTime: '2023/06/15 2:30 PM', status: 'APPROVED', ktas: 'KTAS2' },
-    { id: '9876', ageGroup: 'INFANT', gender: 'MALE', tags: 'tag1', address: '321 봉명동', callingTime: '2023/04/16 3:45 PM', replyTime: '2023/04/15 4:15 PM', status: 'APPROVED', ktas: 'KTAS1' },
-    { id: '1234', ageGroup: 'YOUTH', gender: 'MALE', tags: '과다출혈', address: '456 유성온천', callingTime: '2023/02/15 10:30 AM', replyTime: '2023/02/15 11:45 AM', status: 'APPROVED', ktas: 'KTAS4' },
-    { id: '5678', ageGroup: 'SENIOR', gender: 'FEMALE', tags: 'tag3', address: '789 금호동', callingTime: '2023/10/11 2:00 PM', replyTime: '2023/06/15 2:30 PM', status: 'APPROVED', ktas: 'KTAS2' },
-    { id: '9876', ageGroup: 'INFANT', gender: 'MALE', tags: 'tag1', address: '321 봉명동', callingTime: '2023/04/16 3:45 PM', replyTime: '2023/04/15 4:15 PM', status: 'APPROVED', ktas: 'KTAS1' }
-  ];
-
-  return Promise.resolve({data, count:21});
+  return Promise.resolve({data:response?.data, count:21});
 }
 
 const DataGrid = () => {
@@ -82,8 +80,8 @@ const DataGrid = () => {
 
   return (
     <div>
-      <h3>Remote filtering & sorting example with pagination</h3>
-      <div style={{ height: 80 }} >Current filterValue: {filterValue ? <code>{JSON.stringify(filterValue, null, 2)}</code> : 'none'}.</div>
+      {/* <h3>Remote filtering & sorting example with pagination</h3>
+      <div style={{ height: 80 }} >Current filterValue: {filterValue ? <code>{JSON.stringify(filterValue, null, 2)}</code> : 'none'}.</div> */}
       <ReactDataGrid
         idProperty="id"
         style={gridStyle}
