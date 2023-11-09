563dbf2d468221fe29283dbf36fb0d5de6f8c033
이승종 <maeugel_rosh@naver.com>
2023-11-09 16:57:55 +0900

    feat: 멤버별 확정된 요청 리스트

--- backend/calling-service/src/main/java/com/codesmith/goojangcalling/calling/persistence/CallingRepository.java
+++ backend/calling-service/src/main/java/com/codesmith/goojangcalling/calling/persistence/CallingRepository.java
@@ -4,6 +4,8 @@ import com.codesmith.goojangcalling.calling.persistence.domain.Calling;
 import com.codesmith.goojangcalling.calling.persistence.domain.Occurrence;
 import com.codesmith.goojangcalling.calling.persistence.domain.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,4 +13,7 @@ public interface CallingRepository extends JpaRepository<Calling, Long>, Calling
     List<Calling> findAllByOccurrence(Occurrence occurrence);
 
     Boolean existsByOccurrenceAndStatus(Occurrence occurrence, Status status);
+
+    @Query(value = "select c from Calling c where c.status = 'FIXED'  and c.occurrence.memberId in :memberList")
+    List<Calling> findAllByMemberIdListAndStatus(@Param("memberList") List<Long> memberList);
 }
