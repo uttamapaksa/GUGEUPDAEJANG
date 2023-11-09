b786a4607af23c6452d4db3512caee2285c260c5
suyeonsu <agew0423@daum.net>
2023-11-10 03:01:54 +0900

    feat: 클라이언트에 소속 안전센터 정보 조회 메서드 등록

--- backend/transfer-service/src/main/java/com/codesmith/goojangtransfer/infra/openfeign/MemberServiceClient.java
+++ backend/transfer-service/src/main/java/com/codesmith/goojangtransfer/infra/openfeign/MemberServiceClient.java
@@ -1,6 +1,9 @@
 package com.codesmith.goojangtransfer.infra.openfeign;
 
 import com.codesmith.goojangtransfer.member.dto.response.MemberInfoResponse;
+import com.codesmith.goojangtransfer.global.passport.MemberInfo;
+import com.codesmith.goojangtransfer.global.passport.presentation.AuthMember;
+import com.codesmith.goojangtransfer.transfer.dto.response.SafetyCenterInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +13,10 @@ import java.util.List;
 
 @FeignClient(name = "MEMBER-SERVICE")
 public interface MemberServiceClient {
+    @GetMapping("/member/hospital")
+    List<String> getPassport(@RequestBody String str);
 
-    @GetMapping("/member/{memberId}")
-    MemberInfoResponse getMember(@PathVariable("memberId") Long memberId);
+    @GetMapping("/member/my-safety-center")
+    SafetyCenterInfoResponse getSafetyCenterInfo(@AuthMember MemberInfo memberInfo);
 }
 
