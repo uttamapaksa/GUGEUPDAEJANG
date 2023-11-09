817a7db6327ac67533e7ba364cf28cbd632a8f3c
이시영 <swy05123@gmail.com>
2023-11-09 14:21:35 +0900

    feat: 병원 메인 페이지 수정 S09P31B204-346

--- frontend/src/components/libraries/Map/InfoWindow/ParamedicTransferInfo.tsx
+++ frontend/src/components/libraries/Map/InfoWindow/ParamedicTransferInfo.tsx
@@ -15,19 +15,30 @@ function ParamedicTransferInfo(props: any) {
                 }
                 else {
                     lonlat = new Tmapv3.LatLng(props.paraTransferList[i].data.latitude, props.paraTransferList[i].data.longitude);
-                }                const infoWindow = new Tmapv3.InfoWindow({
+                }
+                const infoWindow = new Tmapv3.InfoWindow({
                     position: lonlat,
                     offset: new Tmapv3.Point(0, -30),
-                    content: renderToString(
-                        <InfoContents
-                            id={props.paraTransferList[i].id}
-                            ktas={props.paraTransferList[i].data.ktas}
-                            addr={props.paraTransferList[i].data.address}
-                            requestAt={props.paraTransferList[i].data.createdAt}
-                            elapseMin={"-"}
-                            leftTime={props.paraTransferList[i].data.duration}
-                        />
-                    ),
+                    content:
+                        props.paraTransferList[i].state === "wait" ? renderToString(
+                            <InfoContents
+                                id={props.paraTransferList[i].id}
+                                ktas={props.paraTransferList[i].data.ktas}
+                                addr={props.paraTransferList[i].data.address}
+                                distance={props.paraTransferList[i].data.distance}
+                                requestAt={props.paraTransferList[i].data.createdAt}
+                                leftTime={props.paraTransferList[i].data.duration}
+                            />
+                        ) : renderToString(
+                            <InfoContents
+                                id={props.paraTransferList[i].id}
+                                ktas={props.paraTransferList[i].data.ktas}
+                                addr={props.paraTransferList[i].curAddr}
+                                distance={props.paraTransferList[i].leftDist}
+                                requestAt={props.paraTransferList[i].data.createdAt}
+                                leftTime={props.paraTransferList[i].leftTime}
+                            />
+                        ),
                     border: '0px solid #ffffff',
                     type: 2,
                     map: props.map
