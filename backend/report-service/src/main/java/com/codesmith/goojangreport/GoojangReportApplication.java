09a2dcadc34ca2f471380a1aac7eac89d5e6ec56
이승종 <maeugel_rosh@naver.com>
2023-11-10 17:12:01 +0900

    init: report 서비스 프로젝트 생성

---
+++ backend/report-service/src/main/java/com/codesmith/goojangreport/GoojangReportApplication.java
@@ -0,0 +1,13 @@
+package com.codesmith.goojangreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoojangReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoojangReportApplication.class, args);
+	}
+
+}
