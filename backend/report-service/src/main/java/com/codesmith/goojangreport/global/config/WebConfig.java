1eabba2c9ea9895f243ed34f776813de1310160a
이승종 <maeugel_rosh@naver.com>
2023-11-11 16:57:13 +0900

    feat: 패스포트, 오픈페인 기본 설정 S09P31B204-420

---
+++ backend/report-service/src/main/java/com/codesmith/goojangreport/global/config/WebConfig.java
@@ -0,0 +1,33 @@
+package com.codesmith.goojangreport.global.config;
+
+import com.codesmith.goojangreport.global.passport.presentation.AuthMemberArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthMemberArgumentResolver authMemberArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authMemberArgumentResolver);
+    }
+
+    @Bean
+    public HttpMessageConverters customConverters() {
+        List<HttpMessageConverter<?>> converters = new ArrayList<>();
+        converters.add(new MappingJackson2HttpMessageConverter());
+        return new HttpMessageConverters(converters);
+    }
+}
