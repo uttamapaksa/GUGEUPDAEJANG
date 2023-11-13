c1420b2d637025f336d4a083baba5044bb6f5d8c
이원영 <sheltonwon@gmail.com>
2023-11-13 10:57:15 +0900

    feat: 생성 listener 완료

--- backend/report-service/src/main/java/com/codesmith/goojangreport/infra/kafka/CallingConsumer.java
+++ backend/report-service/src/main/java/com/codesmith/goojangreport/infra/kafka/CallingConsumer.java
@@ -1,5 +1,7 @@
 package com.codesmith.goojangreport.infra.kafka;
 
+import com.codesmith.goojangreport.report.dto.message.CallingCreateMessage;
+import com.codesmith.goojangreport.report.dto.message.CallingStatusMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,10 +12,19 @@ import org.springframework.stereotype.Service;
 public class CallingConsumer {
     private final ObjectMapper objectMapper;
 
-    @KafkaListener(topics = "calling-topic", groupId = "report-consumer")
+    @KafkaListener(topics = "calling-topic", groupId = "report-consumer-dev")
     public void consumeCallingTopic(String message){
         try {
-            System.out.println(message);
+            System.out.println("========================" + message);
+            if (message.contains("occurrenceId")) {
+                CallingCreateMessage callingCreateMessage = objectMapper.readValue(message, CallingCreateMessage.class);
+                System.out.println(callingCreateMessage);
+            }
+
+            else {
+                CallingStatusMessage callingStatusMessage = objectMapper.readValue(message, CallingStatusMessage.class);
+                System.out.println(callingStatusMessage);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
---
+++ backend/report-service/src/main/java/com/codesmith/goojangreport/report/application/ReportServiceImpl.java
@@ -0,0 +1,22 @@
+package com.codesmith.goojangreport.report.application;
+
+import com.codesmith.goojangreport.report.dto.message.CallingCreateMessage;
+import com.codesmith.goojangreport.report.dto.message.CallingStatusMessage;
+import com.codesmith.goojangreport.report.dto.message.TransferMessage;
+
+public class ReportServiceImpl implements ReportService {
+    @Override
+    public void createReport(CallingCreateMessage callingCreateMessage) {
+
+    }
+
+    @Override
+    public void updateReport(CallingStatusMessage callingStatusMessage) {
+
+    }
+
+    @Override
+    public void updateReport(TransferMessage transferMessage) {
+
+    }
+}
