780ee50a61878ddccc71ef6055b7a86713a65afb
이승종 <maeugel_rosh@naver.com>
2023-11-13 13:56:30 +0900

    test: 연령대별 리포트 테스트

--- backend/report-service/src/main/java/com/codesmith/goojangreport/report/presentation/ReportController.java
+++ backend/report-service/src/main/java/com/codesmith/goojangreport/report/presentation/ReportController.java
@@ -3,7 +3,9 @@ package com.codesmith.goojangreport.report.presentation;
 import com.codesmith.goojangreport.global.passport.MemberInfo;
 import com.codesmith.goojangreport.global.passport.presentation.AuthMember;
 import com.codesmith.goojangreport.report.application.ReportService;
+import com.codesmith.goojangreport.report.dto.reponse.AgeGroupResponse;
 import com.codesmith.goojangreport.report.dto.reponse.CallingPerTimeResponse;
+import com.codesmith.goojangreport.report.dto.reponse.DailyStatusResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,9 +24,27 @@ public class ReportController {
     private final ReportService reportService;
 
     @GetMapping("/time")
-    public ResponseEntity<CallingPerTimeResponse> getDailyTimeReport(@AuthMember MemberInfo memberInfo) {
+    public ResponseEntity<CallingPerTimeResponse> getTimeGroupReport(@AuthMember MemberInfo memberInfo) {
         Long[] arr = {440L, 505L, 414L, 671L, 227L, 413L, 201L, 352L, 352L, 752L, 320L, 257L};
         List<Long> list = new ArrayList<>(Arrays.asList(arr));
         return ResponseEntity.ok(new CallingPerTimeResponse(list));
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<DailyStatusResponse> getDailyStatusReport(@AuthMember MemberInfo memberInfo) {
+        Long[] arr = {44L, 55L, 13L, 43L, 22L, 30L};
+        List<Long> list = new ArrayList<>(Arrays.asList(arr));
+        return ResponseEntity.ok(new DailyStatusResponse(list));
+    }
+
+    @GetMapping("/age")
+    public ResponseEntity<AgeGroupResponse> getAgeGroupReport(@AuthMember MemberInfo memberInfo) {
+        Long[] arr1 = {280L, 290L, 330L, 360L, 320L, 330L, 290L, 330L};
+        Long[] arr2 = {160L, 180L, 190L, 150L, 250L, 200L, 170L, 220L};
+        Long[] arr3 = {120L, 110L, 140L, 210L, 170L, 130L, 120L, 110L};
+        ArrayList<Long> list1 = new ArrayList<>(Arrays.asList(arr1));
+        ArrayList<Long> list2 = new ArrayList<>(Arrays.asList(arr2));
+        ArrayList<Long> list3 = new ArrayList<>(Arrays.asList(arr3));
+        return ResponseEntity.ok(new AgeGroupResponse(list1, list2, list3));
     }
 }
