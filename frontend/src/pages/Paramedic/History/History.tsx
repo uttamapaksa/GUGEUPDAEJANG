14db4900fb18fc26ba068214b0f6e9aa929f93e8
김준섭 <vacanter@naver.com>
2023-11-13 10:33:42 +0900

    tmp

--- frontend/src/pages/Paramedic/History/History.tsx
+++ frontend/src/pages/Paramedic/History/History.tsx
@@ -1,5 +1,24 @@
+import * as S from './History.style';
+import A from '/src/components/Commons/Atoms';
+
 function History() {
-  return <></>;
+  const goToPrev = () => {
+    console.log(123)
+  }
+
+  return (
+    <>
+    <S.ParamedicHeader>
+      <S.Arrow>
+        <A.ImgArrowLeft $width="2vh" onClick={goToPrev} />
+      </S.Arrow>
+      <A.TxtHeaderTitle>이송 기록</A.TxtHeaderTitle>
+      <S.Arrow>
+      </S.Arrow>
+    </S.ParamedicHeader>
+    
+    </>
+  );
 }
 
 export default History;
