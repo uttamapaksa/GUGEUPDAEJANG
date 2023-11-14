9f37e05a34333a2037ef21e9db2509dd22826bcd
김수연 <agew0423@daum.net>
2023-11-14 17:08:32 +0900

    fix: 응답 dto에 setter 추가

--- backend/report-service/src/main/java/com/codesmith/goojangreport/report/dto/reponse/DailyKtasResponse.java
+++ backend/report-service/src/main/java/com/codesmith/goojangreport/report/dto/reponse/DailyKtasResponse.java
@@ -3,10 +3,12 @@ package com.codesmith.goojangreport.report.dto.reponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class DailyKtasResponse {
