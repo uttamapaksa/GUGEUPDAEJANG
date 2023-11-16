0cc7ea6c06372038b7740897973915bfd2d1b0d9
이승종 <maeugel_rosh@naver.com>
2023-11-16 09:20:26 +0900

    env: 타임존 설정

--- backend/report-service/src/main/java/com/codesmith/goojangreport/GoojangReportApplication.java
+++ backend/report-service/src/main/java/com/codesmith/goojangreport/GoojangReportApplication.java
@@ -5,11 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class GoojangReportApplication {
 
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GoojangReportApplication.class, args);
 	}
