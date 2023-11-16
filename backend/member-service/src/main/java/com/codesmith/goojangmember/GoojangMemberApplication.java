0746f027bc152a039d400e4c19cfc14ecde028d6
이승종 <maeugel_rosh@naver.com>
2023-11-16 09:25:25 +0900

    fix: 오타수정

--- backend/member-service/src/main/java/com/codesmith/goojangmember/GoojangMemberApplication.java
+++ backend/member-service/src/main/java/com/codesmith/goojangmember/GoojangMemberApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.TimeZone
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
