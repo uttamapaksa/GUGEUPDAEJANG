fee046c2b3a09f02c95be7e6f8836139f86af156
이승종 <maeugel_rosh@naver.com>
2023-11-16 09:22:17 +0900

    env: 타임존 설정

--- backend/transfer-service/src/main/java/com/codesmith/goojangtransfer/GoojangTransferApplication.java
+++ backend/transfer-service/src/main/java/com/codesmith/goojangtransfer/GoojangTransferApplication.java
@@ -8,10 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class GoojangTransferApplication {
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoojangTransferApplication.class, args);
